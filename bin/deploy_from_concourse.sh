6d2e46382e95cc82f3225934ad6b3ec4583c3e01
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-25 08:48:03 -0700

    out/bosh-init will be in a gopath

--- bin/deploy_from_concourse.sh
+++ bin/deploy_from_concourse.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
 
 export PATH=$PWD/bin:$PATH
-export PATH=$PWD/build-bosh-init/out:$PATH
+export PATH=$PWD/build-bosh-init/gopath/src/github.com/cloudfoundry/bosh-init/out:$PATH
 
 bosh-init deploy redis.yml \
   stemcell/stemcell.tgz \
